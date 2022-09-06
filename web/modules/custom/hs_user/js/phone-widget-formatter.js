4ee64f177fd28563d568e2364d41125d915a0908
Samuel Dufrin <samueldufrin@Samuels-MacBook-Pro.local>
2022-09-06 11:05:18 -0400

    HS-144: JS fix

--- web/modules/custom/hs_user/js/phone-widget-formatter.js
+++ web/modules/custom/hs_user/js/phone-widget-formatter.js
@@ -2,13 +2,15 @@ const phoneFields = document.querySelectorAll('.input--phone');
 
 const formatInput = (input) => {
     let targetValue = input.value.replaceAll('-', '');
+    let lastSelection = input.selectionStart;
     let newNum = '';
     for(let i = 0; i < targetValue.length; i++){
-        (i === 2 || i === 5)
-            ? newNum += targetValue[i] + '-'
-            : newNum += targetValue[i];
+        (i === 3 || i === 6) ? newNum += '-' + targetValue[i] : newNum += targetValue[i];
     }
+    (lastSelection === 4 && targetValue.length === 4) ? lastSelection++ : null;
+    (lastSelection === 8 && targetValue.length === 7) ? lastSelection++ : null;
     input.value = newNum;
+    input.setSelectionRange(lastSelection, lastSelection);
 }
 
 phoneFields.forEach((input) => {
